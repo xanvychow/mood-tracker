b88cb2af56c93024038efc392f0eb431abc30eb7
xanvy <xanvychow@gmail.com>
2021-11-28 09:41:14 +0000

    EntryMOdalDone

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,19 @@
 <script>
     import Greeting from '$lib/Greeting.svelte';
     import Entry from '$lib/Entry.svelte';
-    function myNewEntry() {
-        var element = document.createElement("Entry");
-        document.body.appendChild(element);
-    }
-    function myNewEntry() {
-        var newEntry = document.createElement("Entry");
-        document.getElementById("entryHere").appendChild(newEntry);
-    }
+    import EntryModal from '$lib/EntryModal.svelte';
 </script>
 <!-- Entries -->
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" onclick="myNewEntry" value="+ New Entry" />
+   	 <input class="btn btn-light mb-2" type="button" onclick="myNewEntry" value="+ New Entry" data-bs-toggle="modal" data-bs-target="#newEntry" />
     </div>
 
-    <div class="list-group mb-3" id="entryHere">
+    <div class="list-group mb-3">
    	 <!-- Individual Entries -->
     	<Entry />
+        <EntryModal />
    	 
     </div>
 </section>
